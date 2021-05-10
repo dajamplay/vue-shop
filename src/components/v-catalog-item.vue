148542c79dc0425f6ffee36161d16f15a03caad1
dajamplay <ggyoug@gmail.cpm>
2021-05-10 19:54:10 +0300

    init base components

---
+++ src/components/v-catalog-item.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="v-catalog-item">
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "v-catalog-item"
+    }
+</script>
+
+<style>
+
+</style>
---
+++ src/components/v-main-wrapper.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="v-main-wrapper">
+        <p>{{title}}</p>
+        <v-catalog></v-catalog>
+        <v-cart></v-cart>
+    </div>
+</template>
+
+<script>
+    import vCatalog from './v-catalog'
+    import vCart from './v-cart'
+
+    export default {
+        name: 'v-main-wrapper',
+        components: {
+            vCatalog,
+            vCart
+        }, //дочерние компоненты
+        props: {}, //то что прилетает из родителя
+        data() { //данные для текущего компонента
+            return {
+                title: 'Main Wrapper'
+            }
+        },
+        computed: {}, //вычисляемые свойства
+        methods: {}, //клики анимации и тд
+        watch: {}, //следить за чем либо
+        mounted() { //когда компонент загрузится
+            console.log("v-main-wrapper is loaded!");
+        }
+    }
+</script>
+
+<style>
+    .v-main-wrapper {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        max-width: 900px;
+        margin: 0 auto;
+    }
+</style>
